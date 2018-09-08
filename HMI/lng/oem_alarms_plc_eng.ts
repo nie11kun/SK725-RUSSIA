3c61c43708c9f77c876cd2788134039f4e6305e8
Marco Nie <nie11kun@outlook.com>
2018-09-08 12:25:47 +0700

    english alarm msg

---
+++ HMI/lng/oem_alarms_plc_eng.ts
@@ -0,0 +1,302 @@
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>slaeconv</name>
+    <message>
+      <source>700000/PLC/828D</source>
+      <translation>Please check the emergency stop button！     →【14512[10]_[20].0】。</translation>
+    </message>
+    <message>
+      <source>700001/PLC/828D</source>
+      <translation>Overload of grinding wheel motor!      →【14512[10]_[20].1】。</translation>
+    </message>
+    <message>
+      <source>700002/PLC/828D</source>
+      <translation>Dressing wheel motor open overload！      →【14512[10]_[20].2】。</translation>
+    </message>
+    <message>
+      <source>700003/PLC/828D</source>
+      <translation>Overload of hydraulic motor.        →【14512[10]_[20].3】。</translation>
+    </message>
+    <message>
+      <source>700004/PLC/828D</source>
+      <translation>Hydrostatic motor overload.      →【14512[10]_[20].4】。</translation>
+    </message>
+    <message>
+      <source>700005/PLC/828D</source>
+      <translation>Intermittent lubrication pump overload.     →【14512[10]_[20].5】。</translation>
+    </message>
+    <message>
+      <source>700006/PLC/828D</source>
+      <translation>Grinding cooling (internal grinding) pump overload.      →【14512[10]_[20].6】。</translation>
+    </message>
+    <message>
+      <source>700007/PLC/828D</source>
+      <translation>Dressing cooling pump overload.          →【14512[10]_[20].7】。</translation>
+    </message>
+    <message>
+      <source>700008/PLC/828D</source>
+      <translation>Shower or External grinding cooling pump overload.  →【14512[11]_[21].0】。</translation>
+    </message>
+    <message>
+      <source>700009/PLC/828D</source>
+      <translation>custom1                 →【14512[11]_[21].1】。</translation>
+    </message>
+    <message>
+      <source>700010/PLC/828D</source>
+      <translation>Overload of cooling water (oil) box.   →【14512[11]_[21].2】。</translation>
+    </message>
+    <message>
+      <source>700011/PLC/828D</source>
+      <translation>External grinding wheel motor overload.     →【14512[11]_[21].3】。</translation>
+    </message>
+    <message>
+      <source>700012/PLC/828D</source>
+      <translation>custom2                 →【14512[11]_[21].4】。</translation>
+    </message>
+    <message>
+      <source>700013/PLC/828D</source>
+      <translation>Static pressure oil cooler overload.      →【14512[11]_[21].5】。</translation>
+    </message>
+    <message>
+      <source>700014/PLC/828D</source>
+      <translation>Spindle water cooler overload.           →【14512[11]_[21].6】。</translation>
+    </message>
+    <message>
+      <source>700015/PLC/828D</source>
+      <translation>Spindle cooling pump overload.           →【14512[11]_[21].7】。</translation>
+    </message>
+    <message>
+      <source>700016/PLC/828D</source>
+      <translation>Headgear lubrication pump overload.           →【14512[12]_[22].0】。</translation>
+    </message>
+    <message>
+      <source>700017/PLC/828D</source>
+      <translation>Intermittent lubrication pump 2 overload.        →【14512[12]_[22].1】。</translation>
+    </message>
+    <message>
+      <source>700018/PLC/828D</source>
+      <translation>Spindle overload of milling cutter .            →【14512[12]_[22].2】。</translation>
+    </message>
+    <message>
+      <source>700019/PLC/828D</source>
+      <translation>Control transformer overload.           →【14512[12]_[22].3】。</translation>
+    </message>
+    <message>
+      <source>700020/PLC/828D</source>
+      <translation>custom3                  →【14512[12]_[22].4】。</translation>
+    </message>
+    <message>
+      <source>700021/PLC/828D</source>
+      <translation>Internal grinding wheel motor overload.      →【14512[12]_[22].5】。</translation>
+    </message>
+    <message>
+      <source>700022/PLC/828D</source>
+      <translation>Overload of drive power.           →【14512[12]_[22].6】。</translation>
+    </message>
+    <message>
+      <source>700023/PLC/828D</source>
+      <translation>Overload overload of grinding wheel 2 Motor.          →【14512[12]_[22].7】。</translation>
+    </message>
+    <message>
+      <source>700024/PLC/828D</source>
+      <translation>Grinding wheel 2 frequency converter alarm !       →【14512[13]_[23].0】。</translation>
+    </message>
+    <message>
+      <source>700025/PLC/828D</source>
+      <translation>custom                 →【14512[13]_[23].1】。</translation>
+    </message>
+    <message>
+      <source>700026/PLC/828D</source>
+      <translation>Grinding wheel frequency converter alarm !   →【14512[13]_[23].2】。</translation>
+    </message>
+    <message>
+      <source>700027/PLC/828D</source>
+      <translation>Motorized spindle temperature alarm !   →【14512[13]_[23].3】。</translation>
+    </message>
+    <message>
+      <source>700028/PLC/828D</source>
+      <translation>Trim wheel drive alarm !   →【14512[13]_[23].4】。</translation>
+    </message>
+    <message>
+      <source>700029/PLC/828D</source>
+      <translation>Driver temperature alarm !   →【14512[13]_[23].5】。</translation>
+    </message>
+    <message>
+      <source>700030/PLC/828D</source>
+      <translation>The drive is not ready.       →【14512[13]_[23].6】。</translation>
+    </message>
+    <message>
+      <source>700031/PLC/828D</source>
+      <translation>Spindle alarm for milling cutter !      →【14512[13]_[23].7】。</translation>
+    </message>
+    <message>
+      <source>700032/PLC/828D</source>
+      <translation>Static pressure oil cooler alarm !    →【14512[14]_[24].0】。</translation>
+    </message>
+    <message>
+      <source>700033/PLC/828D</source>
+      <translation>Water cooling device alarm !      →【14512[14]_[24].1】。</translation>
+    </message>
+    <message>
+      <source>700034/PLC/828D</source>
+      <translation>Cooling tank alarm !      →【14512[14]_[24].2】。</translation>
+    </message>
+    <message>
+      <source>700035/PLC/828D</source>
+      <translation>Low pressure alarm !      →【14512[14]_[24].3】。</translation>
+    </message>
+    <message>
+      <source>700036/PLC/828D</source>
+      <translation>High pressure alarm !      →【14512[14]_[24].4】。</translation>
+    </message>
+    <message>
+      <source>700037/PLC/828D</source>
+      <translation>Internal grinding wheel transducer alarm ! →【14512[14]_[24].5】。</translation>
+    </message>
+    <message>
+      <source>700038/PLC/828D</source>
+      <translation>External grinding wheel transducer alarm ! →【14512[14]_[24].6】。</translation>
+    </message>
+    <message>
+      <source>700039/PLC/828D</source>
+      <translation>Abnormal air pressure alarm !      →【14512[14]_[24].7】。</translation>
+    </message>
+    <message>
+      <source>700040/PLC/828D</source>
+      <translation>Asynchronous subroutine 1 output error !     →【14512[15]_[25].0】。</translation>
+    </message>
+    <message>
+      <source>700041/PLC/828D</source>
+      <translation>Spindle cooling water not detected !     →【14512[15]_[25].1】。</translation>
+    </message>
+    <message>
+      <source>700042/PLC/828D</source>
+      <translation>Spindle oil mist lubrication was not detected !  →【14512[15]_[25].2】。</translation>
+    </message>
+    <message>
+      <source>700043/PLC/828D</source>
+      <translation>No headgear lubricant was detected !     →【14512[15]_[25].3】。</translation>
+    </message>
+    <message>
+      <source>700044/PLC/828D</source>
+      <translation>Insufficient hydraulic pressure !          →【14512[15]_[25].4】。</translation>
+    </message>
+    <message>
+      <source>700045/PLC/828D</source>
+      <translation>Insufficient static pressure !           →【14512[15]_[25].5】。</translation>
+    </message>
+    <message>
+      <source>700046/PLC/828D</source>
+      <translation>custom4                  →【14512[15]_[25].6】。</translation>
+    </message>
+    <message>
+      <source>700047/PLC/828D</source>
+      <translation>custom5                  →【14512[15]_[25].7】。</translation>
+    </message>
+    <message>
+      <source>700048/PLC/828D</source>
+      <translation>Driver not loaded enable !          ◇→【14512[26].0】。</translation>
+    </message>
+    <message>
+      <source>700049/PLC/828D</source>
+      <translation>Please note that the magnification is "0".   ◇→【14512[26].1】。</translation>
+    </message>
+    <message>
+      <source>700050/PLC/828D</source>
+      <translation>Hydraulic motor not started !            ◇ →【14512[26].2】。</translation>
+    </message>
+    <message>
+      <source>700051/PLC/828D</source>
+      <translation>Please start the lubricating pump !          ◇→【14512[26].3】。</translation>
+    </message>
+    <message>
+      <source>700052/PLC/828D</source>
+      <translation>The chuck is in loose state.        ◇→【14512[16]_[26].4】。</translation>
+    </message>
+    <message>
+      <source>700053/PLC/828D</source>
+      <translation>Watch out! The probe is sticking out!        ◇→【14512[16]_[26].5】。</translation>
+    </message>
+    <message>
+      <source>700054/PLC/828D</source>
+      <translation>The clamping device of the A axis is in loose state.     ◇→【14512[16]_[26].6】。</translation>
+    </message>
+    <message>
+      <source>700055/PLC/828D</source>
+      <translation>The safety door is not closed and cannot start the program !  ◇→【14512[16]_[26].7】。</translation>
+    </message>
+    <message>
+      <source>700056/PLC/828D</source>
+      <translation>The grinding wheel has not reached the set speed !      ◇→【14512[17]_[27].0】。</translation>
+    </message>
+    <message>
+      <source>700057/PLC/828D</source>
+      <translation>The dressing wheel has not reached the set speed !      ◇→【14512[17]_[27].1】。</translation>
+    </message>
+    <message>
+      <source>700058/PLC/828D</source>
+      <translation>The swing cylinder did not return to its starting position !       ◇→【14512[17]_[27].2】。</translation>
+    </message>
+    <message>
+      <source>700059/PLC/828D</source>
+      <translation>Mist absorption motor  overload .            ◇→【14512[17]_[27].3】。</translation>
+    </message>
+    <message>
+      <source>700060/PLC/828D</source>
+      <translation>The DRF function is effective. State X1.      ◇→【14512[27].4】。</translation>
+    </message>
+    <message>
+      <source>700061/PLC/828D</source>
+      <translation>The DRF function is effective. State X10.    ◇→【14512[27].5】。</translation>
+    </message>
+    <message>
+      <source>700062/PLC/828D</source>
+      <translation>The DRF function is effective. State X100.    ◇→【14512[27].6】。</translation>
+    </message>
+    <message>
+      <source>700063/PLC/828D</source>
+      <translation>The machine shaft is not back to the reference point !           ◇→【14512[27].7】。</translation>
+    </message>
+    <message>
+      <source>700064/PLC/828D</source>
+      <translation>排屑器过载Overload of chip remover .                  ◇→【14512[18]_[28].0】。</translation>
+    </message>
+    <message>
+      <source>700065/PLC/828D</source>
+      <translation>Overload of electric box air conditioner .              ◇→【14512[18]_[28].1】。</translation>
+    </message>
+    <message>
+      <source>700066/PLC/828D</source>
+      <translation>Electric box air conditioning alarm !             ◇→【14512[18]_[28].2】。</translation>
+    </message>
+    <message>
+      <source>700067/PLC/828D</source>
+      <translation>Magnetic separator overload .             ◇→【14512[18]_[28].3】。</translation>
+    </message>
+    <message>
+      <source>700068/PLC/828D</source>
+      <translation>The X axis has not been completed back to zero, and the Z axis can not return to zero.   ◇→【14512[18]_[28].4】。</translation>
+    </message>
+    <message>
+      <source>700069/PLC/828D</source>
+      <translation>Spindle 1/2 speed has reached the limit speed, check the grinding wheel speed .    ◇→【14512[18]_[28].5】。</translation>
+    </message>
+    <message>
+      <source>700071/PLC/828D</source>
+      <translation> A axis clamping pressure is insufficient .              ◇→【14512[18]_[28].7】。</translation>
+    </message>
+    <message>
+      <source>700072/PLC/828D</source>
+      <translation>Intermittent lubrication pump low liquid level .          ◇→【14512[19]_[29].0】。</translation>
+    </message>
+    <message>
+      <source>700073/PLC/828D</source>
+      <translation>Intermittent lubrication pump 2 low liquid level .       ◇→【14512[19]_[29].1】。</translation>
+    </message>
+    <message>
+      <source>700074/PLC/828D</source>
+      <translation>Data storage is in progress…</translation>
+    </message>
+  </context>
+</TS>
